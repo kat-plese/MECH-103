f4b33df4f4eada4f0a6c2e934fd862293560d9f6
Katie Plese <83364963+katie-plese@users.noreply.github.com>
2021-04-29 21:58:44 -0600

    Update untrasonic_distance.m

--- Ultrasonic Distance/untrasonic_distance.m
+++ Ultrasonic Distance/untrasonic_distance.m
@@ -1,11 +1,9 @@
-% File Name: Ultrasonic 9
-% Name: Katie Plese
-% Date: 28 October 2020
-
+% MACHEN SIE SAUBER
 clear all
 close all
 clc
 
+% CREATE ARDUINO OBJECT
 sensyArduino = arduino('COM3','Uno','libraries','ultrasonic');
 
 TRIG_PIN = 'D5';
@@ -17,54 +15,51 @@ SPEAK_PIN = 'D9';
 playTone(sensyArduino,SPEAK_PIN,440,1);
 duty_cycle = 0.45;
 
-% This while loop will run forever, so we will stick our distance reading,
-% LED lighting, and noise making inside of here
-
+% INFINITE LOOP
 while(1)
-    % Read the distance from the ultrasonic sensor
+    % READ DISTANCE FROM ULTRASONIC SENSOR
     distance = readDistance(ultraSensor)
     
-    % This PIN is shite
     configurePin(sensyArduino, SPEAK_PIN, 'PWM');
     
-    % Use nested if statements to check distance and decide
-    % which lights, if any, need to go on.
+    % SERIES OF NESTED IF STATEMENTS DETERMINE LIGHT CONDITIONS
     if distance > 2
-        writeDigitalPin(sensyArduino, 'D13', 1); % green on
-        writeDigitalPin(sensyArduino, 'D12', 1); % green on
-        writeDigitalPin(sensyArduino, 'D04', 0); % yellow off
-        writeDigitalPin(sensyArduino, 'D07', 0); % red off
-        writeDigitalPin(sensyArduino, 'D02', 0); % red off
+        writeDigitalPin(sensyArduino, 'D13', 1); % GREEN ON
+        writeDigitalPin(sensyArduino, 'D12', 1); % GREEN ON
+        writeDigitalPin(sensyArduino, 'D04', 0); % YELLOW OFF
+        writeDigitalPin(sensyArduino, 'D07', 0); % RED OFF
+        writeDigitalPin(sensyArduino, 'D02', 0); % RED OFF
     elseif distance <= 2 && distance > 1
-        writeDigitalPin(sensyArduino, 'D13', 1); % green on
-        writeDigitalPin(sensyArduino, 'D12', 0); % green off
-        writeDigitalPin(sensyArduino, 'D04', 0); % yellow off
-        writeDigitalPin(sensyArduino, 'D07', 0); % red off
-        writeDigitalPin(sensyArduino, 'D02', 0); % red off
+        writeDigitalPin(sensyArduino, 'D13', 1);
+        writeDigitalPin(sensyArduino, 'D12', 0);
+        writeDigitalPin(sensyArduino, 'D04', 0);
+        writeDigitalPin(sensyArduino, 'D07', 0);
+        writeDigitalPin(sensyArduino, 'D02', 0);
         if distance <= 1.25
-            writeDigitalPin(sensyArduino, 'D13', 1); % green on
-            writeDigitalPin(sensyArduino, 'D12', 0); % green off
-            writeDigitalPin(sensyArduino, 'D04', 1); % yellow on
-            writeDigitalPin(sensyArduino, 'D07', 0); % red off
-            writeDigitalPin(sensyArduino, 'D02', 0); % red off
+            writeDigitalPin(sensyArduino, 'D13', 1);
+            writeDigitalPin(sensyArduino, 'D12', 0);
+            writeDigitalPin(sensyArduino, 'D04', 1);
+            writeDigitalPin(sensyArduino, 'D07', 0);
+            writeDigitalPin(sensyArduino, 'D02', 0);
         end
     elseif distance <= 1 && distance > 0.75
-        writeDigitalPin(sensyArduino, 'D13', 0); % green off
-        writeDigitalPin(sensyArduino, 'D12', 0); % green off
-        writeDigitalPin(sensyArduino, 'D04', 1); % yellow on
-        writeDigitalPin(sensyArduino, 'D07', 1); % red on
-        writeDigitalPin(sensyArduino, 'D02', 0); % red off
+        writeDigitalPin(sensyArduino, 'D13', 0);
+        writeDigitalPin(sensyArduino, 'D12', 0);
+        writeDigitalPin(sensyArduino, 'D04', 1);
+        writeDigitalPin(sensyArduino, 'D07', 1);
+        writeDigitalPin(sensyArduino, 'D02', 0);
     elseif distance <= 0.75 && distance > 0
-        writeDigitalPin(sensyArduino, 'D13', 0); % green off
-        writeDigitalPin(sensyArduino, 'D12', 0); % green off
-        writeDigitalPin(sensyArduino, 'D04', 0); % yellow off
-        writeDigitalPin(sensyArduino, 'D07', 1); % red on
-        writeDigitalPin(sensyArduino, 'D02', 1); % red on
+        writeDigitalPin(sensyArduino, 'D13', 0);
+        writeDigitalPin(sensyArduino, 'D12', 0);
+        writeDigitalPin(sensyArduino, 'D04', 0);
+        writeDigitalPin(sensyArduino, 'D07', 1);
+        writeDigitalPin(sensyArduino, 'D02', 1);
         writePWMDutyCycle(sensyArduino, SPEAK_PIN, duty_cycle);
     end
     
     pause(0.5);
     
+    % RESET LIGHTS
     writeDigitalPin(sensyArduino, 'D13', 0); 
     writeDigitalPin(sensyArduino, 'D12', 0); 
     writeDigitalPin(sensyArduino, 'D04', 0); 
@@ -73,15 +68,16 @@ while(1)
     playTone(sensyArduino,SPEAK_PIN,0,1);
 end
 
-conversion = 0.3048; % meters per foot
+% METERS PER FOOT
+conversion = 0.3048;
 
-% Experimental Meters
-D_1 = 1.6; % measured distance for 1 green light
-D_2 = 1.15; % measured distance for 1 yellow light
-D_3 = 0.95; % measured distance for 1 red light
-D_4 = 0.7; % measured distance for  2 red lights
+% EXPERIMENTAL VALUES
+D_1 = 1.6;  % GREEN LIGHT MEASURED DISTANCE 
+D_2 = 1.15; % YELLOW LIGHT MEASURED DISTANCE
+D_3 = 0.95; % 1 RED LIGHT MEASURED DISTANCE
+D_4 = 0.7;  % 2 RED LIGHTS MEASURED DISTANCE
 
-% Percent Error
+% TRUE PERCENT RELATIVE ERROR
 E_1 = abs((2 - D_1)/2)*100;
 E_2 = abs((1.25 - D_2)/1.25)*100;
 E_3 = abs((1 - D_3)/1)*100;
